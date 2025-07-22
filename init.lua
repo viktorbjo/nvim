d6f053e36f47d6e4a64cc35690d9d57d0c394d05
viktorbjo <viktor.bjorken@icloud.com>
2025-07-22 11:07:46 +0200

    spell

--- init.lua
+++ init.lua
@@ -10,10 +10,8 @@ vim.cmd("colorscheme onedark_dark")
 --
 -- Copy to system clipboard with Ctrl+Shift+C (in visual mode)
 vim.keymap.set('v', '<C-c>', '"+y', { desc = 'Copy to system clipboard' })
-
 -- Copy current line to system clipboard with Ctrl+Shift+C (in normal mode)
 vim.keymap.set('n', '<C-c>', '"+yy', { desc = 'Copy line to system clipboard' })
-
 -- Paste from system clipboard with Ctrl+Shift+V (in normal and insert mode)
 vim.keymap.set('n', '<C-S-v>', '"+p', { desc = 'Paste from system clipboard' })
 vim.keymap.set('i', '<C-S-v>', '<C-r>+', { desc = 'Paste from system clipboard' })
@@ -21,3 +19,14 @@ vim.keymap.set('i', '<C-S-v>', '<C-r>+', { desc = 'Paste from system clipboard' 
 -- line nubers 
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+-- spellcheck 
+
+-- Toggle English spellcheck
+vim.keymap.set('n', '<leader>se', '<cmd>setlocal spell! spelllang=en<CR>', { desc = 'Toggle English spellcheck' })
+
+-- Toggle Swedish spellcheck  
+vim.keymap.set('n', '<leader>ss', '<cmd>setlocal spell! spelllang=sv<CR>', { desc = 'Toggle Swedish spellcheck' })
+
+-- Disable spellcheck
+vim.keymap.set('n', '<leader>sn', '<cmd>setlocal nospell<CR>', { desc = 'Disable spellcheck' })
