ccf2f3ab32efd05de9a8b2e4757aef9282e5b1de
viktorbjo <viktor.bjorken@icloud.com>
2025-07-22 10:29:35 +0200

    clipboard

--- init.lua
+++ init.lua
@@ -3,9 +3,20 @@ require("viktor")
 
 --color theme
 vim.cmd("colorscheme onedark_dark")
+--vim.cmd("colorscheme onelight")
 
 --clipboard 
-vim.opt.clipboard = "unnamedplus"
+--vim.opt.clipboard = "unnamedplus"
+--
+-- Copy to system clipboard with Ctrl+Shift+C (in visual mode)
+vim.keymap.set('v', '<C-c>', '"+y', { desc = 'Copy to system clipboard' })
+
+-- Copy current line to system clipboard with Ctrl+Shift+C (in normal mode)
+vim.keymap.set('n', '<C-c>', '"+yy', { desc = 'Copy line to system clipboard' })
+
+-- Paste from system clipboard with Ctrl+Shift+V (in normal and insert mode)
+vim.keymap.set('n', '<C-S-v>', '"+p', { desc = 'Paste from system clipboard' })
+vim.keymap.set('i', '<C-S-v>', '<C-r>+', { desc = 'Paste from system clipboard' })
 
 -- line nubers 
 vim.wo.number = true
