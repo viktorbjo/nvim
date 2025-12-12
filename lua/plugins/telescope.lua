53bee2117957168749f2cf676ec03b21a9f4ec69
viktorbjo <90506608+viktorbjo@users.noreply.github.com>
2025-12-12 14:07:46 +0000

    Disable Telescope key mappings
    Comment out Telescope key mappings for file finding and grepping.
    remove comments after build

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,8 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fs', function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ")})
-end)
+-- local builtin = require('telescope.builtin')
+-- vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+-- vim.keymap.set('n', '<leader>fs', function() 
+-- 	builtin.grep_string({ search = vim.fn.input("Grep > ")})
+-- end)
 
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
